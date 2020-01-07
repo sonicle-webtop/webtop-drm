701b0e0a764be577718062c1c8a5e2e2b1aa2da3
Gabriele Bulfon <gbulfon@sonicle.com>
2020-01-07 10:57:29 +0100

    fix 5.1.0 sql update

--- src/main/resources/com/sonicle/webtop/drm/meta/db/5.1.0_1.sql
+++ src/main/resources/com/sonicle/webtop/drm/meta/db/5.1.0_1.sql
@@ -1,6 +1,4 @@
-@DataSource[default@com.sonicle.webtop.mail]
-
-/*TRUNCATE TABLE timetable_report_temp;*/
+@DataSource[default@com.sonicle.webtop.drm]
 
 ALTER TABLE "drm"."timetable_report_temp"
 ADD COLUMN "target_user_id" varchar(36) NOT NULL;
