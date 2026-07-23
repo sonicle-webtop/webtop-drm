27dc720f9f744433b14aa03b011fc5cb6a664533
gabriele.bulfon <gabriele.bulfon@sonicle.com>
2026-07-23 15:56:42 +0200

    fix range ferie e malattia non devono riempire i giorni non lavorativi con 8h

--- src/main/java/com/sonicle/webtop/drm/DrmManager.java
+++ src/main/java/com/sonicle/webtop/drm/DrmManager.java
@@ -2984,7 +2984,7 @@ public class DrmManager extends BaseManager implements IDrmManager{
         String hRange = lhDao.selectSumLineHourByHourProfileIdDayOfWeek(con, hp.getId(), ld.getDayOfWeek());
         
         if (hRange == null || hRange.isEmpty() || hRange.equals("0")) {
-            return "8"; // Default to 8 hours
+            return null;
         }
 
         // Convert minutes to hour:minute format
